e202ec0ecabd5b95977474b592661fa0fd6678e9
Serhiy Koziuk <seryjkozyk@gmail.com>
2024-03-08 18:44:07 +0200

    update facade pattern

--- facade/facade.js
+++ facade/facade.js
@@ -1,6 +1,22 @@
-// Facade pattern
-function addEvent(el, ev, fu) {
-    (el.addEventListener) ? el.addEventListener(ev, fu, false) :
-        (el.attachEvent) ? el.attachEvent('on' + ev, fu) :
-            el['on' + ev] = fu;
+/**
+ * Adds an event listener to an element.
+ * @param {Element} el - The element to which the event listener will be added.
+ * @param {string} event - The name of the event to listen for.
+ * @param {function} fn - The callback function to be executed when the event is triggered.
+ */
+function addEvent(el, event, fn) {
+  if (el.addEventListener) {
+    el.addEventListener(event, fn, false);
+  } else if (el.attachEvent) {
+    el.attachEvent("on" + event, fn);
+  } else {
+    el["on" + event] = fn;
+  }
 }
+
+// tests
+const btn = document.querySelector("button");
+
+addEvent(btn, "click", () => {
+  console.log("btn click");
+});
