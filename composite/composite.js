1db9284205c58d60b1f0b55bddf57dd9fa1c3d4b
Serhiy Koziuk <seryjkozyk@gmail.com>
2024-03-08 18:44:46 +0200

    update composite pattern

--- composite/composite.js
+++ composite/composite.js
@@ -1,26 +1,26 @@
 // Composite js pattern
 class SoundsGallery {
-    constructor(name) {
-        this.gallery = [];
-        this.name = name;
-    }
-    // public methods
-    add(child) {
-        this.gallery.push(child);
-    }
-    getSoundsGallery() {
-        console.log(this);
-    }
+  constructor(name) {
+    this.gallery = [];
+    this.name = name;
+  }
+  // public methods
+  add(child) {
+    this.gallery.push(child);
+  }
+  getSoundsGallery() {
+    console.log(this);
+  }
 }
 
 // test
-let root = new SoundsGallery('root'),
-    rock = new SoundsGallery('rock'),
-    metal = new SoundsGallery('metal'),
-    popRock = new SoundsGallery('popRock'),
-    punkRock = new SoundsGallery('punkRock'),
-    alternative = new SoundsGallery('alternative'),
-    nuMetal = new SoundsGallery('nuMetal');
+const root = new SoundsGallery("root"),
+  rock = new SoundsGallery("rock"),
+  metal = new SoundsGallery("metal"),
+  popRock = new SoundsGallery("popRock"),
+  punkRock = new SoundsGallery("punkRock"),
+  alternative = new SoundsGallery("alternative"),
+  nuMetal = new SoundsGallery("nuMetal");
 
 root.add(rock);
 root.add(metal);
