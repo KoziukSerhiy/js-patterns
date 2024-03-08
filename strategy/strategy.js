58f176d1146c3c4b4074d851158ed81de3f320b1
Serhiy Koziuk <seryjkozyk@gmail.com>
2024-03-08 17:39:37 +0200

    upgrade strategy pattern

--- strategy/strategy.js
+++ strategy/strategy.js
@@ -1,41 +1,73 @@
+/**
+ * The `Validator` class is responsible for validating data using different strategies.
+ * It allows the user to set a strategy and then use that strategy to check the validity of a given set of parameters.
+ */
 class Validator {
+  /**
+   * Initializes the `Validator` object with a null strategy.
+   */
   constructor() {
-    this.strategy = "";
+    /**
+     * Holds the current validation strategy.
+     * @type {Object}
+     */
+    this.strategy = null;
   }
 
+  /**
+   * Sets the validation strategy to be used.
+   * @param {Object} strategy - The validation strategy to be set.
+   */
   setStrategy(strategy) {
     this.strategy = strategy;
   }
 
+  /**
+   * Calls the `check` method of the current strategy to validate the given parameters.
+   * @param {Object} params - The parameters to be validated.
+   * @returns {boolean} - True if the parameters are valid, false otherwise.
+   */
   check(params) {
     return this.strategy.check(params);
   }
 }
 
-// strategys
+// strategies
 class StringStrategy {
-  check(s) {
-    typeof s === "string"
-      ? console.log(`It's a string`)
-      : console.log(`Not a string`);
+  check(string) {
+    if (typeof string === "string") {
+      console.log(`It's a string`);
+
+      return true;
+    } else {
+      console.log(`Not a string`);
+
+      return false;
+    }
   }
 }
 
 class NumberStrategy {
-  check(n) {
-    typeof n === "number"
-      ? console.log(`It's a number`)
-      : console.log(`Not a number`);
+  check(number) {
+    if (typeof number === "number") {
+      console.log(`It's a number`);
+
+      return true;
+    } else {
+      console.log(`Not a number`);
+
+      return false;
+    }
   }
 }
 
 // test
-const name = "Tom";
-const age = 18;
+const userName = "Tom";
+const userAge = 18;
 
 const validator = new Validator();
 
 validator.setStrategy(new StringStrategy());
-validator.check(name);
+validator.check(userName);
 validator.setStrategy(new NumberStrategy());
-validator.check(age);
+validator.check(userAge);
