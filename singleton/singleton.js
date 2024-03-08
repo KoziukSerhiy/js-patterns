e9735a2d14cb37e68529f84b19c1fccfc7e6cc2d
Serhiy Koziuk <seryjkozyk@gmail.com>
2024-03-08 18:05:51 +0200

    upgrade a singleton pattern

--- singleton/singleton.js
+++ singleton/singleton.js
@@ -1,31 +1,54 @@
-// singletone
-class CreateInstance {
-    constructor() {
-        this.counter = 0;
+/**
+ * The `Counter` class is a singleton class that allows for counting and manipulating a single count value.
+ * It ensures that only one instance of the class is created and provides methods to increment, decrement, and retrieve the count value.
+ */
+class Counter {
+  /**
+   * Initializes the count value to 0 and sets the instance of the class as a static property.
+   * It ensures that only one instance of the class is created.
+   */
+  constructor() {
+    if (!Counter.instance) {
+      /**
+       * @type {number} The current count value.
+       */
+      this.count = 0;
+      Counter.instance = this;
     }
-    increaseCounter() {
-        this.counter++;
-    }
-    getCounter() {
-        return this.counter;
-    }
+
+    return Counter.instance;
+  }
+
+  /**
+   * Increments the count value by 1.
+   */
+  increment() {
+    this.count++;
+  }
+
+  /**
+   * Decrements the count value by 1.
+   */
+  decrement() {
+    this.count--;
+  }
+
+  /**
+   * Returns the current count value.
+   * @returns {number} The current count value.
+   */
+  getCount() {
+    return this.count;
+  }
 }
 
-var CounterModule = (function () {
-    var instance = null;
-
-    return function () {
-        return instance || (instance = new CreateInstance());
-    };
-})();
-
 // Tests
-var coun1 = new CounterModule();
+const counter_1 = new Counter();
 
-console.log(coun1.getCounter()); // 0
-coun1.increaseCounter();
+console.log(counter_1.getCount()); // 0
+counter_1.increment();
 
-var coun2 = new CounterModule();
-console.log(coun2.getCounter()); // 1
+const counter_2 = new Counter();
 
-console.log(coun1 === coun2); // true
+console.log(counter_2.getCount()); // 1
+console.log(counter_1 === counter_2); // true
